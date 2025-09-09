0d703f21ae1aaa4f0d4b43654f213e9136c499d7
Maksud Alam <maksud.patna@gmail.com>
2025-09-09 22:33:10 +0530

    :wrench: CONFIG : mongo config added

--- backend/config/data.js
+++ backend/config/data.js
@@ -6,7 +6,7 @@ import mongoose from "mongoose"
         await mongoose.connect("mongodb://localhost:27017/virtual")
 
         console.log("connect succesfully")
-     }
+     }                  
      catch{
     console.log("connect db failed")
      }
